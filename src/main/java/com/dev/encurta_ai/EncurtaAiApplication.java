c3387de4a649d366e50dcdd334ed9a5489abe039
alantrs <alanaraujotrs@gmail.com>
2024-08-19 21:26:53 -0300

    docs: adicionado swagger

--- src/main/java/com/dev/encurta_ai/EncurtaAiApplication.java
+++ src/main/java/com/dev/encurta_ai/EncurtaAiApplication.java
@@ -1,7 +1,21 @@
 package com.dev.encurta_ai;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Tag(name = "URL Shortener", description = "Endpoints for URL shortening service")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "URL Shortener",
+				version = "1.0",
+				description = "API to shorten url",
+				contact = @Contact(name = "Alan Araujo Rodrigues", email = "alanaraujotrs@gmail.com")
+		)
+)
 
 @SpringBootApplication
 public class EncurtaAiApplication {
--- src/main/java/com/dev/encurta_ai/controller/LinkController.java
+++ src/main/java/com/dev/encurta_ai/controller/LinkController.java
@@ -3,6 +3,11 @@ package com.dev.encurta_ai.controller;
 import com.dev.encurta_ai.dto.LinkResponse;
 import com.dev.encurta_ai.model.Link;
 import com.dev.encurta_ai.service.LinkService;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,23 +20,26 @@ import java.util.Map;
 
 @RestController
 @CrossOrigin("*")
+@Tag(name = "Link Controller")
 public class LinkController {
 
     @Autowired
     private LinkService linkService;
 
     @PostMapping("/encurta-ai")
+    @Operation(summary = "Shorten a URL")
     public ResponseEntity<LinkResponse> createLink(@RequestParam String urlOriginal){
         LinkResponse linkResponse = linkService.createLink(urlOriginal);
         return ResponseEntity.status(HttpStatus.CREATED).body(linkResponse);
     }
 
     @GetMapping("/r/{urlShort}")
+    @Hidden
     public ResponseEntity<Void> redirect(@PathVariable String urlShort, HttpServletRequest request, HttpServletResponse response) {
         String urlOriginal = linkService.recoverUrlOriginal(urlShort, request);
         try {
             response.sendRedirect(urlOriginal);
-            return ResponseEntity.status(HttpStatus.OK).build();
+            return ResponseEntity.status(HttpStatus.FOUND).build();
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
